2a2a990ab10e7cc0c0afd10aebc0b53ce021e364
Serhat <serhat16_@hotmail.com>
2023-09-28 14:59:36 +0300

    Add db settings.

--- SurveyApp.UI/Program.cs
+++ SurveyApp.UI/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SurveyApp.Data.DataBase;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("SqlConnection"), Action => {
+        Action.MigrationsAssembly("SurveyApp.Data");
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
