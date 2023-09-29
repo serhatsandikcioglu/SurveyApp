b16022e496fb7e2090e3e9da203d272287ee06fc
Serhat <serhat16_@hotmail.com>
2023-09-29 12:25:18 +0300

    Add mapper.

---
+++ SurveyApp.Service/Mapper/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SurveyApp.Data.DTO_s;
+using SurveyApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApp.Service.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile() 
+        { 
+            CreateMap<Question, QuestionDTO>().ReverseMap();
+        }
+        
+    }
+}
