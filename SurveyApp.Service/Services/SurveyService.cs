b7bc5f5992e4b5e545fb9735f2be174a9e7e1fcc
Serhat <serhat16_@hotmail.com>
2023-10-02 14:34:20 +0300

    Add survey repository,service and pages.

---
+++ SurveyApp.Service/Services/SurveyService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using SurveyApp.Data.DTO_s;
+using SurveyApp.Data.Entities;
+using SurveyApp.Data.Interfaces;
+using SurveyApp.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApp.Service.Services
+{
+    public class SurveyService : ISurveyService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public SurveyService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public void Add(SurveyDTO survey)
+        {
+            var mappedSurvey = _mapper.Map<Survey>(survey);
+            _unitOfWork.SurveyRepository.Add(mappedSurvey);
+            _unitOfWork.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SurveyDTO GetById(Guid id)
+        {
+            var survey = _unitOfWork.SurveyRepository.GetById(id);
+            var surveyDTO = _mapper.Map<SurveyDTO>(survey);
+            surveyDTO.Questions = new List<QuestionDTO>();
+            foreach (var questionId in surveyDTO.QuestionsId)
+            {
+                var question = _unitOfWork.QuestionRepository.GetById(questionId);
+                var questionDTO = _mapper.Map<QuestionDTO>(question);
+                surveyDTO.Questions.Add(questionDTO);
+            }
+            return surveyDTO;
+        }
+    }
+}
--- SurveyApp.UI/Controllers/SurveyController.cs
+++ SurveyApp.UI/Controllers/SurveyController.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SurveyApp.Data.DataBase;
 using SurveyApp.Data.DTO_s;
+using SurveyApp.Data.Entities;
 using SurveyApp.Service.Interfaces;
+using SurveyApp.UI.Models;
 
 namespace SurveyApp.UI.Controllers
 {
     public class SurveyController : Controller
     {
         private readonly IQuestionService _questionService;
+        private readonly ISurveyService _surveyService;
+        private readonly IScoreService _scoreService;
 
-        public SurveyController(IQuestionService questionService)
+        public SurveyController(IQuestionService questionService, ISurveyService surveyService, IScoreService scoreService)
         {
             _questionService = questionService;
+            _surveyService = surveyService;
+            _scoreService = scoreService;
         }
 
         public IActionResult Index()
@@ -20,14 +27,54 @@ namespace SurveyApp.UI.Controllers
             return View(questions);
         }
         [HttpPost]
-        public IActionResult CreateSurvey()
+        public IActionResult CreateSurvey(SurveyDTO survey)
         {
-            return RedirectToAction();
+            _surveyService.Add(survey);
+            return RedirectToAction("Survey", new { id = survey.Id });
+        }
+
+        public IActionResult Survey(Guid id)
+        {
+            var survey = _surveyService.GetById(id);
+            
+            return View(survey);
         }
         [HttpPost]
-        public IActionResult AddQuestionToSurvey()
+        public IActionResult CreateScore(ScoreDTO score)
         {
-            return RedirectToAction("Suvey");
+            _scoreService.Add(score);
+            return RedirectToAction("ScoreResult" , new { id = score.Id });
         }
+        public IActionResult ScoreResult(Guid id)
+        {
+            var score =_scoreService.GetById(id);
+            return View(score);
+        }
+        public IActionResult SurveyResults(Guid id)
+        {
+           var scores = _scoreService.GetScoresBySurvey(id);
+            return View(scores);
+        }
+        public IActionResult GetSurveyResults(string surveyLink)
+        {
+            try
+            {
+                Uri uri = new Uri(surveyLink);
+                string guid = uri.Segments.Last();
+                string surveyResultPageUrl = "/Survey/SurveyResults/" + guid;
+
+                return Redirect(surveyResultPageUrl);
+            }
+            catch (UriFormatException)
+            {
+                return View("FindSurveyResults");
+            }
+            
+        }
+        public IActionResult FindSurveyResults()
+        {
+            return View();
+        }
+
     }
 }
