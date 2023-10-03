f83911dfb3d46e55970fd10e2e3b562b29d51590
Serhat <serhat16_@hotmail.com>
2023-10-03 12:22:33 +0300

    Add entity config.

---
+++ SurveyApp.Service/Configurations/EntityConfiguration/QuestionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SurveyApp.Data.Entities;
+
+namespace SurveyApp.Service.Configurations.EntityConfiguration
+{
+    public class QuestionConfiguration : IEntityTypeConfiguration<Question>
+    {
+        public void Configure(EntityTypeBuilder<Question> builder)
+        {
+            builder.ToTable("Question");
+            builder.Property(x => x.Text).IsRequired();
+            builder.Property(x => x.Choices).IsRequired();
+            builder.Property(x => x.IsConfirmed).IsRequired();
+
+        }
+    }
+}
---
+++ SurveyApp.Service/Configurations/EntityConfiguration/ScoreConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SurveyApp.Data.Entities;
+
+namespace SurveyApp.Service.Configurations.EntityConfiguration
+{
+    public class ScoreConfiguration : IEntityTypeConfiguration<Score>
+    {
+        public void Configure(EntityTypeBuilder<Score> builder)
+        {
+            builder.ToTable("Score");
+            builder.Property(x => x.SurveyId).IsRequired();
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Surname).IsRequired();
+            builder.Property(x => x.NumberOfCorrectAnswer).IsRequired();
+                
+            builder.HasOne(c => c.Survey).WithMany().HasForeignKey(c => c.SurveyId).OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
